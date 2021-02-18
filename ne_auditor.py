def3a9000aeb96ce95829866c7e05bbb8695bf67
APolkhirev <polkhirev@ya.ru>
2021-02-18 13:37:18 +0300

    Мелкие правки

--- ne_auditor.py
+++ ne_auditor.py
@@ -1,11 +1,10 @@
 # Скрипт написан для частных задач, решаемых в конкретном проекте и не является универсальным инструментом (пока).
-#def print_hi(name):
-#    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-with open('ne_list.txt', 'r') as reader:
-    # Read & print the entire file
+ne_counter = 0
+with open('./ne_list.txt', 'r') as reader:
     line = reader.readline()
-    while line != '':  # The EOF char is an empty string
+    while line != '':
         print(line, end='')
         line = reader.readline()
+        ne_counter += 1
+print("\n\nЗавершено. обследовано устройств:", ne_counter, "\nПрограмму можно закрыть.")
+input()
