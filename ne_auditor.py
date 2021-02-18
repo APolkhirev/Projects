117c74361a7c902d5045ee40a8f9a825f668cc49
APolkhirev <polkhirev@ya.ru>
2021-02-18 15:24:47 +0300

    Мелкие правки

--- ne_auditor.py
+++ ne_auditor.py
@@ -1,10 +1,21 @@
 # Скрипт написан для частных задач, решаемых в конкретном проекте и не является универсальным инструментом (пока).
 ne_counter = 0
-with open('./ne_list.txt', 'r') as reader:
-    line = reader.readline()
-    while line != '':
-        ne_counter += 1
-        print('NE', ne_counter, ':', line, end='')
+ip_list_file = 'ne_list.txt'
+ip_list_file_exist = False
+try:
+    open(ip_list_file, 'r')
+    ip_list_file_exist = True
+except FileNotFoundError:
+    print("Ошибка: файл", ip_list_file, "не найден.")
+if ip_list_file_exist:
+    with open(ip_list_file, 'r') as reader:
         line = reader.readline()
-print("\n\nDone. Total processed NEs:", ne_counter)
+        while line != '':
+            ne_counter += 1
+            print('NE', ne_counter, ':', line, end='')
+            line = reader.readline()
+    print("\n\nГотово. Всего обработано NE:", ne_counter)
+else:
+    pass
+
 input()
