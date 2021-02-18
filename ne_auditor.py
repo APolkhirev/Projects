d12c75beb9bc6cf7fdde7dbe64f9deb5bf11a7f1
APolkhirev <polkhirev@ya.ru>
2021-02-18 11:49:55 +0300

    Мелкие правки

--- ne_auditor.py
+++ ne_auditor.py
@@ -1,8 +1,7 @@
 # Скрипт написан для частных задач, решаемых в конкретном проекте и не является универсальным инструментом (пока).
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+#def print_hi(name):
+#    # Use a breakpoint in the code line below to debug your script.
+#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 with open('ne_list.txt', 'r') as reader:
     # Read & print the entire file
