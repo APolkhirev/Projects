777ce5eca4deff010963a24b53efa6cba18b3526
APolkhirev <polkhirev@ya.ru>
2021-02-19 11:44:41 +0300

    Мелкие правки

--- ne_auditor.py
+++ ne_auditor.py
@@ -1,25 +1,44 @@
 # Скрипт написан для частных задач, решаемых в конкретном проекте и не является универсальным инструментом (пока).
 # Да и может он не много (пока).
+import os
+import shutil
+import datetime
 
-ip_list_file: str = 'ne_list.txt'
-ip_list_file_exist = False
+v_date_time: str = str(datetime.date.today())
+v_ip_list_file: list = 'ne_list.txt'
+v_ip_list_file_exist: bool = False
+v_path = os.getcwd() + '\\audit_result_' + v_date_time
 
 try:
-    open(ip_list_file, 'r')
-    ip_list_file_exist = True
-except FileNotFoundError:
-    print(f"Ошибка: файл ./{ip_list_file}, содержащий список IP-адресов сетевых элементов, не найден.")
+    shutil.rmtree(v_path, ignore_errors=False, onerror=None)
+except OSError:
+    print (f"Удалить директорию result не удалось")
+else:
+    pass
 
-ne_counter = 0
-if ip_list_file_exist:
-    print(f"Список импортирован из файла {ip_list_file}:")
-    line = ' '
-    with open(ip_list_file, 'r') as ipreader:
-        while line:
-            ne_counter += 1
-            line = ipreader.readline()
-            print(f"NE-{ne_counter}: {line}", end='')
-    print(f"\n\nГотово. Всего было обработано NE: {ne_counter}")
+try:
+    os.mkdir(v_path)
+except OSError:
+    print (f"Создать директорию result не удалось")
+else:
+    pass
+
+try:
+    open(v_ip_list_file, 'r')
+    v_ip_list_file_exist = True
+except FileNotFoundError:
+    print(f"Ошибка: файл ./{v_ip_list_file}, содержащий список IP-адресов сетевых элементов, не найден.")
+
+v_ne_counter: int = 0
+if v_ip_list_file_exist:
+    print(f"Список импортирован из файла {v_ip_list_file}:")
+    with open(v_ip_list_file, 'r') as v_ipreader:
+        v_line = v_ipreader.readline()
+        while v_line != '':
+            v_ne_counter += 1
+            print(f"NE-{v_ne_counter}: {v_line}", end='')
+            v_line = v_ipreader.readline()
+    print(f"\n\nГотово. Всего было обработано NE: {v_ne_counter}")
 else:
     pass
 
