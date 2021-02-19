74427881514217633dd52f0cd535dfb109a469e3
APolkhirev <polkhirev@ya.ru>
2021-02-19 23:06:20 +0300

    Мелочь

--- ne_auditor.py
+++ ne_auditor.py
@@ -1,24 +1,21 @@
 # NE_auditor v.01
 # Скрипт написан для частных задач, решаемых в конкретном проекте и не является универсальным инструментом (пока).
 # Да и может он не много (пока).
-import os
-import shutil
 import datetime
 import getpass
-
+import os
+import shutil
+from netmiko import Netmiko
 
 v_login = input("Введите логин аудитора: ")
 try:
     v_pass = getpass.getpass("Введите пароль аудитора: ")
 except Exception as err:
     print('Ошибка: ', err)
-else:
-    pass
 
 v_date_time: str = str(datetime.date.today())
 v_ip_list_file: str = 'ne_list.txt'
 v_commands_file: str = 'ne_commands.txt'
-v_commands_file_exist = v_ip_list_file_exist = False
 v_path: str = './audit_result_' + v_date_time
 v_coms = v_nes = ()  # определяем список команд и список NE
 
@@ -26,54 +23,29 @@ try:
     shutil.rmtree(v_path, ignore_errors=False, onerror=None)
 except OSError:
     print (f"Удалить директорию result не удалось")
-else:
-    pass
 
 try:
     os.mkdir(v_path)
 except OSError:
     print (f"Создать директорию result не удалось")
-else:
-    pass
 
 try:
-    open(v_ip_list_file, 'r')
-    v_ip_list_file_exist = True
+    with open(v_ip_list_file, 'r') as v_ipreader:
+        v_line: str = v_ipreader.readline()
+        while v_line:
+            v_nes = v_nes + (v_line.rstrip(),)
+            v_line = v_ipreader.readline()
 except FileNotFoundError:
     print(f"Ошибка: файл ./{v_ip_list_file}, содержащий построчный список IP-адресов сетевых элементов, не найден.")
 
 try:
-    open(v_commands_file_exist, 'r')
-    v_commands_file_exist = True
-except FileNotFoundError:
-    print(f"Ошибка: файл ./{v_commands_file}, содержащий построчный спи6сок команд, не найден.")
-
-v_counter: int = 0
-if v_commands_file_exist:
-    # print(f"\nСписок импортирован из файла {v_commands_file}:")
     with open(v_commands_file, 'r') as v_commreader:
         v_line: str = v_commreader.readline()
         while v_line:
-            v_counter += 1
             v_coms = v_coms + (v_line.rstrip(),)
-            # print(f"Команда {v_counter}: {v_line}", end='')
             v_line = v_commreader.readline()
-else:
-    pass
-
-
-v_counter: int = 0
-if v_ip_list_file_exist:
-    # print(f"\n\nСписок импортирован из файла {v_ip_list_file}:")
-    with open(v_ip_list_file, 'r') as v_ipreader:
-        v_line: str = v_ipreader.readline()
-        while v_line:
-            v_counter += 1
-            v_nes = v_nes + (v_line.rstrip(),)
-            # print(f"NE-{v_counter}: {v_line}", end='')
-            v_line = v_ipreader.readline()
-else:
-    pass
+except FileNotFoundError:
+    print(f"Ошибка: файл ./{v_commands_file}, содержащий построчный список команд, не найден.")
 
 v_counter: int = 1
 for x in v_nes:
@@ -91,3 +63,21 @@ for x in v_nes:
                 f.close()
 print('\n\n', v_nes)
 print('\n\n', v_coms)
+
+#host1 = {
+#    "host": "192.168.1.1",
+#    "username": "admin",
+#    "password": "auditor_password",
+#    "device_type": "huawei",
+#    "global_delay_factor": 0.1,
+#    }
+#try:
+#    net_connect = Netmiko(**host1)
+#except NoValidConnectionsError:
+#    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1Обломинго")
+#else:
+#    v_command = ["display current config", "display version"]
+#    print("Connected to:", net_connect.find_prompt())
+#    output = net_connect.send_config_set(v_command, delay_factor=.5)
+#    net_connect.disconnect()
+#    print(output)
