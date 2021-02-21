04203c893e01f5a4bbbdc08e62938d43dcf3003e
APolkhirev <polkhirev@ya.ru>
2021-02-21 14:47:36 +0300

    Сплит модулей

--- ne_auditor.py
+++ ne_auditor.py
@@ -7,8 +7,103 @@ import datetime
 import getpass
 import os
 import shutil
-import ff_ipchecker
-import ff_access_to_ne
+import netmiko
+import ipaddress
+
+
+def f_checkip(v_ip):
+    """
+    Проверка валидности IP-адреса для назначения на интерфейсе. Скорипт проверяепт как формат, так и принадлежность
+    зарезервированным пулам адресов, которые не могут использоваться с этой целью.
+    """
+    try:
+        ipaddress.ip_address(v_ip)
+    except (ipaddress.AddressValueError, ValueError) as e:
+        v_ip_description: str = 'Bad format for IP address.'
+        return False, v_ip_description
+    if ipaddress.ip_address('0.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('0.255.255.255'):
+        v_ip_description = "Bad IP: Current network (only valid as source address)."
+        return False, v_ip_description
+    elif ipaddress.ip_address('100.64.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('100.127.255.255'):
+        v_ip_description = """Bad IP: Shared address space for communications between a service provider and its 
+        subscribers when using a carrier-grade NAT."""
+        return False, v_ip_description
+    elif ipaddress.ip_address('127.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('127.255.255.255'):
+        v_ip_description = "Bad IP: Used for loopback addresses to the local host."
+        return False, v_ip_description
+    elif ipaddress.ip_address('169.254.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('169.254.255.255'):
+        v_ip_description = """Bad IP: Used for link-local addresses between two hosts on a single link when no IP 
+        address is otherwise specified, such as would have normally been retrieved from a DHCP server."""
+        return False, v_ip_description
+    elif ipaddress.ip_address('192.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.0.0.255'):
+        v_ip_description = "Bad IP: IETF Protocol Assignments."
+        return False, v_ip_description
+    elif ipaddress.ip_address('192.0.2.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.0.2.255'):
+        v_ip_description = "Bad IP: Assigned as TEST-NET-1, documentation and examples."
+        return False, v_ip_description
+    elif ipaddress.ip_address('192.31.196.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.31.196.255'):
+        v_ip_description = "Bad IP: AS112-v4."
+        return False, v_ip_description
+    elif ipaddress.ip_address('192.52.193.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.52.193.255'):
+        v_ip_description = "Bad IP: AMT."
+        return False, v_ip_description
+    elif ipaddress.ip_address('192.88.99.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.88.99.0'):
+        v_ip_description = "Bad IP: Reserved. Formerly used for IPv6 to IPv4 relay."
+        return False, v_ip_description
+    elif ipaddress.ip_address('192.175.48.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.175.48.0'):
+        v_ip_description = "Bad IP: Direct Delegation AS112 Service."
+        return False, v_ip_description
+    elif ipaddress.ip_address('198.18.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('198.19.255.255'):
+        v_ip_description = """Bad IP: Used for benchmark testing of inter-network communications
+        between two separate subnets."""
+        return False, v_ip_description
+    elif ipaddress.ip_address('198.51.100.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('198.51.100.255'):
+        v_ip_description = "Bad IP: Assigned as TEST-NET-2, documentation and examples."
+        return False, v_ip_description
+    elif ipaddress.ip_address('203.0.113.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('203.0.113.255'):
+        v_ip_description = "Bad IP: Assigned as TEST-NET-3, documentation and examples."
+        return False, v_ip_description
+    elif ipaddress.ip_address('224.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('239.255.255.255'):
+        v_ip_description = "Bad IP: In use for IP multicast. (Former Class D network)."
+        return False, v_ip_description
+    elif ipaddress.ip_address('240.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('255.255.255.254'):
+        v_ip_description = "Bad IP: In use for IP multicast. Reserved for future use. (Former Class E network)."
+        return False, v_ip_description
+    elif ipaddress.ip_address(v_ip) == ipaddress.ip_address('255.255.255.255'):
+        v_ip_description = """Bad IP: Reserved for the "limited broadcast" destination address."""
+        return False, v_ip_description
+    v_ip_description: str = 'IP address is valid'
+    return True, v_ip_description
+
+
+def f_ne_access(v_host_ip, v_username, v_password, v_vendor, v_comsi, v_nediri):
+    """     вывод команд в файл    """
+    v_ne_ssh = {
+        "host": v_host_ip,
+        "username": v_username,
+        "password": v_password,
+        "device_type": v_vendor,
+        # "global_delay_factor": 0.1,  # Increase all sleeps by a factor of 1
+    }
+
+    try:
+        net_connect = netmiko.Netmiko(**v_ne_ssh)
+    except netmiko.ssh_exception.NetmikoTimeoutException:
+        print(f'Не удалось подключиться к NE c IP-адресом {v_ne_ssh["host"]}')
+        return 'Не доступен'
+    else:
+        print("Успешное подключение к:", net_connect.find_prompt())
+        i: int = 0
+        while v_comsi:
+            v_filename: str = f"{v_nediri}" + r"\(" + f"{str(i)})_{v_comsi[i]}.log"
+            with open(v_filename, 'w') as f_output:
+                output = net_connect.send_command_timing(v_comsi[i])
+                f_output.write(output)
+                f_output.close()
+                i += 1
+        net_connect.disconnect()
+        return 'SSH'
+
 
 v_date_time: str = str(datetime.date.today())
 v_ip_list_file: str = 'ne_list.txt'
@@ -34,12 +129,12 @@ try:
         v_counter = 1
         while v_readedip:
             v_counter += 1
-            if ff_ipchecker.f_checkip(v_readedip.rstrip())[0]:
+            if f_checkip(v_readedip.rstrip())[0]:
                 v_nes = v_nes + (v_readedip.rstrip(),)
             else:
                 print(f"Ошибка в строке [{v_counter}] "
                       f"{v_readedip.rstrip()}: "
-                      f"{ff_ipchecker.f_checkip(v_readedip.rstrip())[1]}")
+                      f"{f_checkip(v_readedip.rstrip())[1]}")
             v_readedip = v_ipreader.readline()
         v_nes = sorted(tuple(set(v_nes)))  # сортируем и убираем дублирующиеся IP'шники
 except FileNotFoundError:
@@ -73,8 +168,10 @@ for x in v_nes:
         print(f"Создать директорию не удалось")
     else:
         v_nedir = v_path + '\\' + f"NE-{v_counter} (" + x + ")"
-        v_access_via = ff_access_to_ne.f_ne_access(v_nes[v_counter-1], v_login, v_pass, "huawei", v_coms, v_nedir)
+        v_access_via = f_ne_access(v_nes[v_counter-1], v_login, v_pass, "huawei", v_coms, v_nedir)
         v_counter += 1
         print(v_access_via)
 print('\n\n', v_nes)
 print('\n', v_coms, '\n')
+
+input()
