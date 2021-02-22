58777e61e633a45f0fe5175d51903b8e81e6932b
apolkhirev <polkhirev@ya.ru>
2021-02-22 22:10:58 +0300

    setup.py tuning

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@
 
 from cx_Freeze import setup, Executable
 
-executables = [Executable('ne_auditor.py')]
+executables = [Executable('ne_auditor.py',
+                          targetName='ne_auditor.exe',
+                          icon='favicon.ico',
+                          shortcutName='NE Auditor'),
+               ]
+
+options = {
+    'build_exe': {
+        'include_msvcr': True,
+    }
+}
 
 setup(name='ne_auditor',
       version='0.1',
       description='Auditor for network elements',
-      executables=executables)
+      executables=executables,
+      options=options)
