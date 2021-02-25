8287b2561e2dddeca0a0ba443774c118e7de5d8e
APolkhirev <polkhirev@ya.ru>
2021-02-25 10:27:43 +0300

    мелкие правки

--- ip_list_checker.py
+++ ip_list_checker.py
@@ -15,61 +15,43 @@ def f_checkip(v_ip):
     try:
         ipaddress.ip_address(v_ip)
     except (ipaddress.AddressValueError, ValueError):
-        v_ip_description: str = 'Invalid IP-address format.'
-        return False, v_ip_description
-    if ipaddress.ip_address('0.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('0.255.255.255'):
-        v_ip_description = "Bad IP: Current network (only valid as source address)."
-        return False, v_ip_description
-    elif ipaddress.ip_address('100.64.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('100.127.255.255'):
-        v_ip_description = """Bad IP: Shared address space for communications between a service provider and its 
-        subscribers when using a carrier-grade NAT."""
-        return False, v_ip_description
-    elif ipaddress.ip_address('127.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('127.255.255.255'):
-        v_ip_description = "Bad IP: Used for loopback addresses to the local host."
-        return False, v_ip_description
-    elif ipaddress.ip_address('169.254.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('169.254.255.255'):
-        v_ip_description = """Bad IP: Used for link-local addresses between two hosts on a single link when no IP 
+          return False, "Invalid IP-address format."
+    if ipaddress.IPv4Address(v_ip).is_link_local:
+        return False, """Bad IP: Used for link-local addresses between two hosts on a single link when no IP 
         address is otherwise specified, such as would have normally been retrieved from a DHCP server."""
-        return False, v_ip_description
-    elif ipaddress.ip_address('192.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.0.0.255'):
-        v_ip_description = "Bad IP: IETF Protocol Assignments."
-        return False, v_ip_description
-    elif ipaddress.ip_address('192.0.2.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.0.2.255'):
-        v_ip_description = "Bad IP: Assigned as TEST-NET-1, documentation and examples."
-        return False, v_ip_description
-    elif ipaddress.ip_address('192.31.196.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.31.196.255'):
-        v_ip_description = "Bad IP: AS112-v4."
-        return False, v_ip_description
-    elif ipaddress.ip_address('192.52.193.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.52.193.255'):
-        v_ip_description = "Bad IP: AMT."
-        return False, v_ip_description
-    elif ipaddress.ip_address('192.88.99.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.88.99.0'):
-        v_ip_description = "Bad IP: Reserved. Formerly used for IPv6 to IPv4 relay."
-        return False, v_ip_description
-    elif ipaddress.ip_address('192.175.48.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.175.48.0'):
-        v_ip_description = "Bad IP: Direct Delegation AS112 Service."
-        return False, v_ip_description
-    elif ipaddress.ip_address('198.18.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('198.19.255.255'):
-        v_ip_description = """Bad IP: Used for benchmark testing of inter-network communications
-        between two separate subnets."""
-        return False, v_ip_description
-    elif ipaddress.ip_address('198.51.100.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('198.51.100.255'):
-        v_ip_description = "Bad IP: Assigned as TEST-NET-2, documentation and examples."
-        return False, v_ip_description
-    elif ipaddress.ip_address('203.0.113.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('203.0.113.255'):
-        v_ip_description = "Bad IP: Assigned as TEST-NET-3, documentation and examples."
-        return False, v_ip_description
-    elif ipaddress.ip_address('224.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('239.255.255.255'):
-        v_ip_description = "Bad IP: In use for IP multicast. (Former Class D network)."
-        return False, v_ip_description
-    elif ipaddress.ip_address('240.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('255.255.255.254'):
-        v_ip_description = "Bad IP: In use for IP multicast. Reserved for future use. (Former Class E network)."
-        return False, v_ip_description
+    elif ipaddress.IPv4Address(v_ip).is_loopback:
+        return False, "Bad IP: Used for loopback addresses to the local host."
     elif ipaddress.ip_address(v_ip) == ipaddress.ip_address('255.255.255.255'):
-        v_ip_description = """Bad IP: Reserved for the "limited broadcast" destination address."""
-        return False, v_ip_description
-    v_ip_description: str = 'IP address is valid'
-    return True, v_ip_description
+        return False, """Bad IP: Reserved for the "limited broadcast" destination address."""
+    elif ipaddress.IPv4Address(v_ip).is_multicast:
+        return False, "Bad IP: In use for IP multicast."
+    elif ipaddress.IPv4Address(v_ip).is_unspecified:
+        return False, "Bad IP: The IP address is unspecified."
+    elif ipaddress.ip_address('0.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('0.255.255.255'):
+          return False, "Bad IP: Current network (only valid as source address)."
+    elif ipaddress.ip_address('100.64.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('100.127.255.255'):
+        return False, """Bad IP: Shared address space for communications between a service provider and its 
+        subscribers when using a carrier-grade NAT."""
+    elif ipaddress.ip_address('192.0.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.0.0.255'):
+        return False, "Bad IP: IETF Protocol Assignments."
+    elif ipaddress.ip_address('192.0.2.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.0.2.255'):
+        return False, "Bad IP: Assigned as TEST-NET-1, documentation and examples."
+    elif ipaddress.ip_address('192.31.196.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.31.196.255'):
+        return False, "Bad IP: AS112-v4."
+    elif ipaddress.ip_address('192.52.193.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.52.193.255'):
+        return False, "Bad IP: AMT."
+    elif ipaddress.ip_address('192.88.99.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.88.99.0'):
+        return False, "Bad IP: Reserved. Formerly used for IPv6 to IPv4 relay."
+    elif ipaddress.ip_address('192.175.48.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('192.175.48.0'):
+        return False, "Bad IP: Direct Delegation AS112 Service."
+    elif ipaddress.ip_address('198.18.0.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('198.19.255.255'):
+        return False, """Bad IP: Used for benchmark testing of inter-network communications
+        between two separate subnets."""
+    elif ipaddress.ip_address('198.51.100.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('198.51.100.255'):
+        return False, "Bad IP: Assigned as TEST-NET-2, documentation and examples."
+    elif ipaddress.ip_address('203.0.113.0') < ipaddress.ip_address(v_ip) < ipaddress.ip_address('203.0.113.255'):
+        return False, "Bad IP: Assigned as TEST-NET-3, documentation and examples."
+    return True, "IP address is valid"
 
 
 def f_ip_list_checker(v_ip_list_file):
