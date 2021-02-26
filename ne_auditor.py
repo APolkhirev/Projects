dff91e67b73d5dea35e3a7b2450e36d0b10f8b2e
APolkhirev <polkhirev@ya.ru>
2021-02-26 17:29:46 +0300

    Доработка отчётности

--- ne_auditor.py
+++ ne_auditor.py
@@ -12,7 +12,7 @@ import getpass
 import os
 import shutil
 import argparse
-import pandas as pd  # для табличного отчёта
+import pandas  # для табличного отчёта
 import tqdm
 from netmiko import Netmiko
 from netmiko import ssh_exception
@@ -34,7 +34,10 @@ v_coms = ()  # определяем список команд
 v_nes = f_ip_list_checker(v_ip_list_file)   # определяем список NE
 v_keys = ['hostname', 'ip', 'model', 'version', 'patch', 'status']
 v_ne_status = dict.fromkeys(v_keys)
-v_ne_status_list = dict()
+v_report = []
+dd_com_sw = "Software Version"
+dd_com_pw = "Patch Package Version:"
+dd_com_dw = "display device"
 
 
 try:
@@ -67,7 +70,6 @@ except Exception as err:
     print('Ошибка: ', err)
 
 v_counter: int = 1
-
 with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:
     for v_ne_ip in v_nes:
         v_ne = f"NE-{v_counter}"
@@ -78,20 +80,19 @@ with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:
             "username": v_login,
             "password": v_pass,
             "device_type": 'huawei',
-            # "global_delay_factor": 0.1,  # Increase all sleeps by a factor of 1
+            "global_delay_factor": 0.1,  # Increase all sleeps by a factor of 1
         }
-
-        v_ne_status_list.update({v_ne: v_ne_status})
-        v_ne_status_list[v_ne]['ip'] = v_ne_ip
+        v_report.append(v_ne_status.copy())
+        v_report[v_counter-1]['ip'] = v_ne_ip
 
         try:
             net_connect = Netmiko(**v_ne_ssh)
         except ssh_exception.NetmikoTimeoutException:
-            v_out_msg = f'Не удалось подключиться к NE c IP-адресом {v_ne_ip}. Хост недоступен.'
-            v_ne_status_list[v_ne]['status'] = 'No access'
+            pbar.write(f'Не удалось подключиться к NE c IP-адресом {v_ne_ip}. Хост недоступен.')
+            v_report[v_counter-1]['status'] = f'No access'
         except ssh_exception.NetmikoAuthenticationException:
-            v_out_msg = f'Не удалось подключиться к NE c IP-адресом {v_ne_ip}. Ошибка аутентификации.'
-            v_ne_status_list[v_ne]['status'] = 'Auth. error'
+            pbar.write(f'Не удалось подключиться к NE c IP-адресом {v_ne_ip}. Ошибка аутентификации.')
+            v_report[v_counter-1]['status'] = 'Auth. error'
         else:
             try:
                 os.mkdir(v_path + '\\' + f"NE-{v_counter} (" + v_ne_ip + ")")
@@ -99,22 +100,43 @@ with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:
                 print(f"Создать директорию не удалось")
             else:
                 pass
+
             for i in enumerate(v_coms):
                 v_filename: str = f"{v_nedir}" + r"\(" + f"{v_ne_ssh['host']})_{i[1]}.log"
                 with open(v_filename, 'w') as f_output:
-                    output = net_connect.send_command_timing(i[1])
+                    output = net_connect.send_command_timing(i[1], delay_factor=.5)
                     f_output.write(output)
+                    output_splited = output.split('\n')
                     f_output.close()
                     v_out_msg = f"Успешное подключение к: {net_connect.find_prompt()}. SSH"
 
+            v_report[v_counter - 1]['status'] = 'Ok'
+            ''' Извлекаем hostname '''
+            v_report[v_counter - 1]['hostname'] = net_connect.find_prompt(delay_factor=.5).strip('<>')
+            ''' Извлекаем версмю ПО '''
+            v_temp1 = net_connect.send_command_timing("display "
+                                                      "current-configuration | include !Software", delay_factor=.5)
+            v_report[v_counter - 1]['version'] = v_temp1.split()[-1]
+            ''' Извлекаем версмю патча '''
+            v_temp2 = net_connect.send_command_timing("display "
+                                                      "patch-information", delay_factor=.5)
+            v_temp2_splited = v_temp2.split('\n')
+            v_report[v_counter - 1]['patch'] = v_temp2_splited[1].split(':')[-1]
+            ''' Извлекаем P/N модели '''
+            v_temp3 = net_connect.send_command_timing("display "
+                                                      "device", delay_factor=.5)
+            v_temp3_splited = v_temp3.split('\n')
+            v_report[v_counter - 1]['model'] = v_temp3_splited[0].split('\'')[0]
+
             net_connect.disconnect()
         v_counter += 1
-        pbar.write(v_out_msg)
+
         pbar.update(1)
 
 """ Печать результата табличкой """
-df = pd.DataFrame.from_dict(v_ne_status_list,orient='index')
+df = pandas.DataFrame.from_records(v_report)
 df.fillna('-', inplace=True)
-print('\n',df)
+print('\n', df)
+
 
 input('Готово. Для завершения программы нажмите Enter.')
