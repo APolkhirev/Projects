903db2135de6c5693e33d60ec41dfe58505aa667
apolkhirev <polkhirev@ya.ru>
2021-03-13 12:26:04 +0300

    Сортировка IP, унификация скрипта под разные вендоры

--- ip_list_checker.py
+++ ip_list_checker.py
@@ -67,7 +67,8 @@ def f_ip_list_checker(v_ip_list_file):
                 if f_checkip(v_readedip.rstrip())[0]:
                     v_nes = v_nes + (v_readedip.rstrip(),)
                 else:
-                    print(f"Ошибка в строке {v_counter} (IP {v_readedip.rstrip()}): {f_checkip(v_readedip.rstrip())[1]}")
+                    print(f"Ошибка в строке {v_counter} (IP {v_readedip.rstrip()}): "
+                          f"{f_checkip(v_readedip.rstrip())[1]}")
                 v_readedip = v_ipreader.readline()
             v_list_len = len(v_nes)
             v_nes = sorted(tuple(set(v_nes)), key=ipaddress.IPv4Address)  # дедубликация и сортировка IP-адресов
