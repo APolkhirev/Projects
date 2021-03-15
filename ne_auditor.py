318f27e7b71438893ac6b4bb4868015598b34cb8
apolkhirev <polkhirev@ya.ru>
2021-03-15 23:31:03 +0300

    Изменение структуры

--- ne_auditor.py
+++ ne_auditor.py
@@ -1,5 +1,5 @@
 """
-NE_auditor v.04
+NE_auditor v.05
 Скрипт для аудита сети.
 """
 
@@ -41,6 +41,49 @@ def f_dir_creator(dir_name):
         print(f"Создать директорию {dir_name} не удалось")
 
 
+def f_comand_outputs_to_files(comands_list, ne_ip, directory_name, net_connect):
+    for i in enumerate(comands_list):
+        v_filename: str = f"{directory_name}" + r"\(" + f"{ne_ip})_{i[1]}.log"
+        with open(v_filename, 'w') as f_output:
+            output = net_connect.send_command_timing(i[1], delay_factor=5)
+            f_output.write(output)
+            f_output.close()
+
+
+def f_device_caller(device_list, login, password):
+    counter: int = 0
+    for v_ne_ip in device_list:
+        v_ne = f"NE-{counter}"
+        v_nedir = v_path + '\\' + f"{v_ne} (" + v_ne_ip + ")"
+        v_ne_ssh = {
+            'device_type': 'autodetect',
+            'ip': v_ne_ip,
+            'username': login,
+            'password': password,
+            'conn_timeout': 15
+        }
+        v_report.append(v_ne_status.copy())
+        v_report[counter]['ip'] = v_ne_ssh["ip"]
+
+        try:
+            net_connect = ConnectHandler(**v_ne_ssh)
+        except ssh_exception.NetmikoTimeoutException:
+            pbar.write(f'Не удалось подключиться к {v_ne_ssh["ip"]}. Хост недоступен по SSH.')
+            v_report[counter]['status'] = 'No access'
+        except ssh_exception.NetmikoAuthenticationException:
+            pbar.write(f'Не удалось подключиться к {v_ne_ssh["ip"]}. Ошибка аутентификации.')
+            v_report[counter]['status'] = 'Auth. error'
+        else:
+            guesser = SSHDetect(**v_ne_ssh)
+            v_report[counter]['device_type'] = guesser.autodetect()
+            f_dir_creator(v_path + '\\' + f"NE-{counter} (" + v_ne_ssh["ip"] + ")")
+            f_comand_outputs_to_files(v_coms, v_ne_ssh["ip"], v_nedir, net_connect)
+            ''' Подготовка отчёта '''
+            v_report[counter]['status'] = 'Ok'
+            v_report[counter]['hostname'] = net_connect.find_prompt().strip('<>#')
+        counter += 1
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -68,50 +111,9 @@ if __name__ == '__main__':
     except Exception as err:
         print('Ошибка: ', err)
 
-    v_counter: int = 1
-    with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:
-        for v_ne_ip in v_nes:
-            v_ne = f"NE-{v_counter}"
-            v_nedir = v_path + '\\' + f"{v_ne} (" + v_ne_ip + ")"
-            v_out_msg = ''
-            v_ne_ssh = {
-                'device_type': 'autodetect',
-                'ip': v_ne_ip,
-                'username': v_login,
-                'password': v_pass,
-                'conn_timeout': 15
-             }
-            v_report.append(v_ne_status.copy())
-            v_report[v_counter-1]['ip'] = v_ne_ip
-
-            try:
-                net_connect = ConnectHandler(**v_ne_ssh)
-            except ssh_exception.NetmikoTimeoutException:
-                pbar.write(f'Не удалось подключиться к {v_ne_ip}. Хост недоступен по SSH.')
-                v_report[v_counter-1]['status'] = 'No access'
-            except ssh_exception.NetmikoAuthenticationException:
-                pbar.write(f'Не удалось подключиться к {v_ne_ip}. Ошибка аутентификации.')
-                v_report[v_counter-1]['status'] = 'Auth. error'
-            else:
-                guesser = SSHDetect(**v_ne_ssh)
-                v_report[v_counter - 1]['device_type'] = guesser.autodetect()
-
-                f_dir_creator(v_path + '\\' + f"NE-{v_counter} (" + v_ne_ip + ")")
-#
-                for i in enumerate(v_coms):
-                    v_filename: str = f"{v_nedir}" + r"\(" + f"{v_ne_ip})_{i[1]}.log"
-                    with open(v_filename, 'w') as f_output:
-                        output = net_connect.send_command_timing(i[1], delay_factor=5)
-                        f_output.write(output)
-                        f_output.close()
-                        v_out_msg = f"Успешное подключение к: {net_connect.find_prompt()}. SSH"
-
-                ''' Подготовка отчёта '''
-                v_report[v_counter - 1]['status'] = 'Ok'
-                v_report[v_counter - 1]['hostname'] = net_connect.find_prompt().strip('<>#')
-
-            v_counter += 1
-            pbar.update(1)
+    with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:  # ПОЧИНИТЬ
+        f_device_caller(v_nes, v_login, v_pass)
+        pbar.update(1)
 
     """ Печать результата табличкой """
     df = pandas.DataFrame.from_records(v_report)
