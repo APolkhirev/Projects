ac2f730edc8bbd8e3ea8ce26284f3c7e0ddb80ca
APolkhirev <polkhirev@ya.ru>
2021-03-16 15:05:32 +0300

    Изменение структуры. YAML. Сломался вывод для Huawei.

--- ne_auditor.py
+++ ne_auditor.py
@@ -3,7 +3,7 @@ NE_auditor v.05
 Скрипт для аудита сети.
 """
 
-
+import yaml
 import datetime
 import getpass
 import os
@@ -11,21 +11,18 @@ import shutil
 import argparse
 import pandas  # для табличного отчёта
 import tqdm
+from pprint import pprint
 from netmiko import ssh_exception, ConnectHandler, SSHDetect
 from ip_list_checker import f_ip_list_checker
 
 
 def f_commands_reader(commands_file):
-    coms = ()  # определяем список команд
     try:
         with open(commands_file, 'r') as commreader:
-            v_line: str = commreader.readline()
-            while v_line:
-                coms = coms + (v_line.rstrip(),)
-                v_line = commreader.readline()
-            coms = tuple(set(coms))  # дедубликация команд
+            coms = yaml.safe_load(commreader)
+            pprint(coms)
     except FileNotFoundError:
-        print(f"Ошибка: файл ./{v_commands_file}, содержащий построчный список команд, не найден.")
+        print(f"Ошибка: файл ./{v_commands_file}, в формате YAML, не найден.")
     return coms
 
 
@@ -41,20 +38,24 @@ def f_dir_creator(dir_name):
         print(f"Создать директорию {dir_name} не удалось")
 
 
-def f_comand_outputs_to_files(comands_list, ne_ip, directory_name, net_connect):
-    for i in enumerate(comands_list):
+def f_comand_outputs_to_files(comands_list, ne_ip, directory_name, ssh, dev_type):
+    c_count = 0
+    c_list = tuple(sorted(comands_list[dev_type]))
+    for i in enumerate(c_list):
         v_filename: str = f"{directory_name}" + r"\(" + f"{ne_ip})_{i[1]}.log"
+        print(i[1])
         with open(v_filename, 'w') as f_output:
-            output = net_connect.send_command_timing(i[1], delay_factor=5)
+            output = ssh.send_command_timing(i[1], delay_factor=5)
             f_output.write(output)
             f_output.close()
+        c_count += 1
 
 
-def f_device_caller(device_list, login, password):
+def f_device_caller(device_list, console_camm, login, password):
     counter: int = 0
     for v_ne_ip in device_list:
-        v_ne = f"NE-{counter}"
-        v_nedir = v_path + '\\' + f"{v_ne} (" + v_ne_ip + ")"
+        v_ne = f'NE-{counter}'
+        v_nedir = v_path + '\\' + f'{v_ne} (' + v_ne_ip + ')'
         v_ne_ssh = {
             'device_type': 'autodetect',
             'ip': v_ne_ip,
@@ -63,24 +64,27 @@ def f_device_caller(device_list, login, password):
             'conn_timeout': 15
         }
         v_report.append(v_ne_status.copy())
-        v_report[counter]['ip'] = v_ne_ssh["ip"]
+        v_report[counter]['ip'] = v_ne_ssh['ip']
 
         try:
             net_connect = ConnectHandler(**v_ne_ssh)
-        except ssh_exception.NetmikoTimeoutException:
-            pbar.write(f'Не удалось подключиться к {v_ne_ssh["ip"]}. Хост недоступен по SSH.')
-            v_report[counter]['status'] = 'No access'
         except ssh_exception.NetmikoAuthenticationException:
             pbar.write(f'Не удалось подключиться к {v_ne_ssh["ip"]}. Ошибка аутентификации.')
             v_report[counter]['status'] = 'Auth. error'
+            pbar.update(1)
+        except ssh_exception:
+            pbar.write(f'Не удалось подключиться к {v_ne_ssh["ip"]}. Хост недоступен по SSH.')
+            v_report[counter]['status'] = 'No SSH access'
+            pbar.update(1)
         else:
             guesser = SSHDetect(**v_ne_ssh)
             v_report[counter]['device_type'] = guesser.autodetect()
-            f_dir_creator(v_path + '\\' + f"NE-{counter} (" + v_ne_ssh["ip"] + ")")
-            f_comand_outputs_to_files(v_coms, v_ne_ssh["ip"], v_nedir, net_connect)
-            ''' Подготовка отчёта '''
+            f_dir_creator(v_path + '\\' + f"NE-{counter} ({v_ne_ssh['ip']})")
+            f_comand_outputs_to_files(console_camm, v_ne_ssh['ip'], v_nedir, net_connect, v_report[counter]['device_type'])
             v_report[counter]['status'] = 'Ok'
             v_report[counter]['hostname'] = net_connect.find_prompt().strip('<>#')
+            net_connect.disconnect()
+            pbar.update(1)
         counter += 1
 
 
@@ -90,7 +94,7 @@ if __name__ == '__main__':
     parser.add_argument("-n", "--network-elements-list", action="store", dest="n",
                         help="Файл со списком сетевых элементов (NE)", default="ne_list.txt")
     parser.add_argument("-c", "--command-list", action="store", dest="c",
-                        help="Файл со списком консольных команд для сетевых элементов (NE)", default="ne_commands.txt")
+                        help="Файл со списком консольных команд для сетевых элементов (NE)", default="ne_commands.yaml")
     args = parser.parse_args()
     v_ip_list_file: str = args.n
     v_commands_file: str = args.c
@@ -111,11 +115,10 @@ if __name__ == '__main__':
     except Exception as err:
         print('Ошибка: ', err)
 
-    with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:  # ПОЧИНИТЬ
-        f_device_caller(v_nes, v_login, v_pass)
-        pbar.update(1)
+    with tqdm.tqdm(total=len(v_nes), desc="Обработано NE") as pbar:
+        f_device_caller(v_nes, v_coms, v_login, v_pass)
 
-    """ Печать результата табличкой """
+    """ Вывод отчёта """
     df = pandas.DataFrame.from_records(v_report)
     df.fillna('-', inplace=True)
     print('\n', df)
