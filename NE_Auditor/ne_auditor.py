18f8af1dd7ce8bd1754e5bd594768b44732d4920
apolkhirev <polkhirev@ya.ru>
2021-03-24 07:30:51 +0300

    UFO constant

--- NE_Auditor/ne_auditor.py
+++ NE_Auditor/ne_auditor.py
@@ -23,6 +23,7 @@ from ip_list_checker import f_ip_list_checker
 
 
 MAX_CONCURRENT_SESSIONS = 10
+DEFAULT_UFO_DEVICE_TYPE = "eltex"
 
 
 def f_commands_reader(commands_file):
@@ -91,7 +92,7 @@ def f_send_commands_to_device(
             device["device_type"] = v_dtype
         else:
             if ufo_type == "":
-                device["device_type"] = "eltex"
+                device["device_type"] = DEFAULT_UFO_DEVICE_TYPE
                 v_dtype = "UFO"
             else:
                 device["device_type"] = ufo_type
